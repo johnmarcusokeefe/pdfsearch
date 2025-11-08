bb02b124113f0d9ba15a990ab345313ed2982ace
John O'Keefe <johnmarcusokeefe@gmail.com>
2025-11-08 13:51:38 +1100

    Initial commit

---
+++ controller.py
@@ -0,0 +1,296 @@
+# source pdfsearch/bin/activate
+import sys, os, mimetypes, img2pdf, io
+from PIL import Image
+from PIL import ImageEnhance
+from pypdf import *
+import Levenshtein as levenshtein
+import ocrmypdf
+from datetime import datetime
+from pdf2image import convert_from_path
+
+import view
+
+from PySide6.QtWidgets import QApplication
+
+#
+# Subclass QMainWindow to customize your application's main window
+#
+class MainController:
+
+    def __init__(self, file_path, file_list):
+        
+        self.file_path = file_path
+        self.file_list = file_list
+
+    # check the pdf is text searchable if not enable ocr button
+    #
+    def count_pdf_pages(self, file_path):
+        try:
+            reader = PdfReader(file_path)
+            return len(reader.pages)
+        except Exception as e:
+                print(f"Error saving file: {e}")
+        
+    #
+    # returns number of text searchable pages
+    #
+    def check_pdf(self, file_path):   
+        # You can now open and process the file content
+        reader = PdfReader(file_path)
+        num_pages = len(reader.pages)
+        for page_num in range(num_pages):
+            page = reader.pages[page_num]
+            text = page.extract_text()
+            # search text on page and if text found create an array of page numbers
+            if text == "":
+                return 0
+            else:
+                #self.page_count_label.setText(f"Page count: {num_pages}")
+                return num_pages
+    #
+    #
+    #
+    def check_if_ocr_required(self, view, is_text_plus_num_pages):
+        if is_text_plus_num_pages == 0:
+            self.ocr_file(view, view.file_path)
+            view.search_pdf_button.setEnabled(True)  
+            print("ocr complete ready for search")
+        else:
+            self.page_number_input.setEnabled(True)
+            view.search_pdf_button.setEnabled(True)
+            view.terminal_log(f"File page count: {is_text_plus_num_pages}")
+            view.page_number_input.clear()
+            # for page in range(is_text_plus_num_pages):
+            #     self.page_number_input.addItem(str(page))
+            #self.page_number_input.addItems(self.pages)
+
+            print("check_if_ocr_required", str(self.pages))
+            pages = self.check_pdf(view.file_path)
+            view.page_count_label.setText(f"Page count: {pages}")
+    #       
+    # carry out a levenshtein ratio search on one word
+    #
+    def fuzzy_pdf_search(self, view, text, search_word, level):
+        l_ratio = 0
+        for word in text.split():
+            l_ratio = levenshtein.ratio(search_word.lower(), word.lower())
+            #print(type(l_ratio))
+            if l_ratio > float(level):
+                prt_string = f"word matched: {word}"
+                view.terminal_log.append(prt_string)
+                #print(f"word in: {search_word.lower()} | word found: {word.lower()} | lev ratio: {round(float(l_ratio), 2)}")
+                return l_ratio
+    #
+    # search through each page and carry out a fuzzy search logging found works
+    # found words and page number are added to an array to allow pages to be 
+    # extracted to one file
+    #
+    def pdf_search(self, view, file_path, search_word, level):
+        print("pdf search", file_path, search_word, level)
+        fuzzy_max = 0.0
+        fuzzy_total = 0.0
+        view.status_bar_label.setText("Searching pdf for matches")
+        page_list = []
+        # in view if page list is greater than 0 the save button will be enabled
+        reader = PdfReader(file_path)
+        num_pages = len(reader.pages)
+        # loop through pages
+        view.terminal_log.append(f"start of search for: {search_word} at level {level}")
+        for page_num in range(num_pages):
+            page = reader.pages[page_num]
+            text = page.extract_text()
+            fuzzy_result = self.fuzzy_pdf_search(view, text, search_word, level)
+            if fuzzy_result != None:
+                if fuzzy_result > fuzzy_max:
+                    fuzzy_max = fuzzy_result
+            else:
+                fuzzy_result = 0
+                
+            if fuzzy_result > 0:
+                fuzzy_total = fuzzy_total + fuzzy_result
+                page_list.append(page_num)
+                view.save_pdf_button.setEnabled(True)
+            # search text on page and if text found create an array of page numbers
+                # if text.lower().find(search_string.get().lower()) != -1:
+                #     print("text found on page ", page_num)
+        print(page_list)
+        view.search_found_label.setText("No Matches Found")
+        if len(page_list) > 0:
+            fuzzy_average = round(fuzzy_total/len(page_list), 1)
+            view.search_found_label.setText(f"Highest match is {str(fuzzy_max)} and average match is {str(fuzzy_average)}")
+            view.status_bar_label.setText(f"search matched {len(page_list)}")
+        else:
+            view.status_bar_label.setText("no results found")
+        return page_list
+
+    #
+    # create a text searchable document
+    #
+    def ocr_file(self, view, file_path):
+    
+        """
+        Adds an OCR text layer to a scanned PDF, making it searchable.
+        """
+        output_pdf_path = "output/ocr_"+os.path.basename(file_path)
+        view.status_bar_label.setText("ocr pdf")
+        ocrmypdf.ocr(file_path, output_pdf_path, skip_text=True, oversample=300, clean=True)
+        
+        print(f"OCR completed. Searchable PDF saved to: {output_pdf_path}")
+        #view.output_label.setText(output_pdf_path)
+        view.set_file_path(output_pdf_path)
+        view.search_pdf_button.setEnabled(True)
+
+    #
+    # todo: option to attach to existing file
+    #
+    def save_pdf(self, view, search_string, page_list):
+        # all files saved to output
+        file_path = view.file_path
+        view.status_bar_label.setText("save pdf")
+        now = datetime.now()
+        print("save pdf", file_path)
+        print("page list array", page_list)
+        reader = PdfReader(file_path)
+        if len(page_list) > 0:
+            writer = PdfWriter()
+            for page in page_list:
+                writer.add_page(reader.pages[page])
+            try:
+                out_path = f"output/{search_string}_{int(now.timestamp())}.pdf"
+                with open(out_path, "wb") as output_pdf:
+                    writer.write(output_pdf)
+                view.terminal_log.append(f"extracted pdf pages saved: {out_path}")
+                view.output_file_label.setText(f"Output Path: {out_path}")
+            except Exception as e:
+                view.terminal_log.append(f"Error saving file: {e}")
+        else:
+            view.terminal_log.append("no files in list save_pdf")
+            
+        view.save_pdf_button.setEnabled(False)
+    #
+    # extract pages
+    #
+    def extract_pdfs(self, view, file_path, page_list):
+        # Open the original PDF file
+        output_dir = os.path.dirname(file_path)
+        extract_to_dir = "/"+os.path.basename(file_path).rsplit('.', 1)[0]
+        # Create the directory if it doesn't exist
+        if not os.path.exists(output_dir+extract_to_dir):
+            os.makedirs(output_dir+extract_to_dir) # os.makedirs creates intermediate directories too
+
+        print("extract", output_dir)
+        try:
+            reader = PdfReader(file_path)
+            num_pages = len(page_list) 
+            for i in range(num_pages):
+                writer = PdfWriter()
+                # get the value from the page list
+                ind = page_list[i]
+                writer.add_page(reader.pages[ind])
+                output_pdf_path = f"{output_dir+extract_to_dir}/{ind}.pdf"  # Naming convention for output files
+                with open(output_pdf_path, "wb") as output_file:
+                    writer.write(output_file)
+                print(f"Page {ind} extracted and saved as {output_pdf_path}")
+                view.terminal_log.append(f"Page {ind} extracted and saved as {output_pdf_path}")
+            return output_pdf_path
+
+        except FileNotFoundError:
+            print(f"Error: The file '{file_path}' was not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            
+    #
+    # merge pdfs
+    #
+    def merge_pdfs(self, view, file_name):
+        
+        pdf_ext = ".pdf"
+        view.status_bar_label.setText("merging pdf")
+        file_list = view.file_list
+        if pdf_ext.lower() in file_name.lower():
+            output_filename = file_name
+        else:
+            output_filename = file_name+pdf_ext
+        print("call merge pdfs")
+        """
+        Merges a list of PDF files into a single output PDF.
+
+        """
+        merger = PdfWriter()
+        for path in file_list:
+            
+            print("merge pdf path: ",path)
+            mime_type = mimetypes.guess_type(path)[0]
+            print(mime_type)
+
+            # tests image type and converts
+            if(mime_type == "image/png" or mime_type == "image/jpeg" or mime_type == "image/jpg"):
+                converted_path = self.image_to_pdf(self, path)
+                merger.append(converted_path)
+            else:
+                merger.append(path)
+            #    
+            try:
+                with open(output_filename, "wb") as output_file:
+                    merger.write(output_file)
+            except Exception as e:
+                print(f"Error saving file: {e}")
+        merger.close()
+        view.terminal_log.append(f"PDFs merged successfully:\n{output_filename}")
+        
+        print(f"PDFs merged successfully into {output_filename}")
+    #
+    #
+    #
+    def pdf_to_image(self, view, dpi_in, fmt_in, brightness=0.99):
+
+        # Store Pdf with convert_from_path function
+        images = convert_from_path(view.file_path, dpi=dpi_in, fmt=fmt_in)
+
+        for i in range(len(images)):
+            # Save pages as images in the pdf
+            enhancer = ImageEnhance.Brightness(images[i])
+            adj_image = enhancer.enhance(brightness) # factor > 1 for brighter, < 1 for darker
+            adj_image.save('output/images/page'+str(i)+"."+fmt_in)
+            view.terminal_log.append('saved: output/images/page'+str(i)+"."+fmt_in)
+        
+        print("extracted images", len(images))
+        return "output/images", len(images)
+    #
+    #
+    #
+    def image_to_pdf(self, img_path):
+        pdf_path = img_path+".pdf"
+        
+        try:
+        # Open the image using Pillow (PIL)
+            image = Image.open(img_path)
+
+        # Convert the image to PDF bytes using img2pdf
+            bytes_to_merge = img2pdf.convert(image.filename)
+            
+        # Write the PDF bytes to a file
+            # with open(pdf_path, "wb") as f:
+            #     f.write(pdf_bytes)
+
+            # print(f"Successfully converted '{img_path}' to '{pdf_path}'")
+        except FileNotFoundError:
+            print(f"Error: Image file not found at '{img_path}'")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            # Close the image if it was opened successfully
+            if 'image' in locals() and image:
+                image.close()
+            # one page
+            return PdfReader(io.BytesIO(bytes_to_merge))  
+
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    window = view.MainWindow(None)
+    window.show()
+
+    app.exec()
